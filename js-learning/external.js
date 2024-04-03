83572e49dcb3a8eb0df976469e650e0361221017
sinkinrin <1569675101@qq.com>
2024-04-03 17:35:02 +0800

    ‘null’

--- js-learning/external.js
+++ js-learning/external.js
@@ -76,6 +76,10 @@ class StringOperations {
     trim() {
         return this.str.trim();
     }
+
+    search(substring) {
+        return this.str.search(substring);
+    }
 }
 
 let str = "Hello, World!";
@@ -86,11 +90,40 @@ console.log(stringOps.toLowerCase()); // 小写
 console.log(stringOps.getLength()); // 字符串的长度，Output: 13
 console.log(stringOps.startsWith("Hello")); // 判断字符串是否以指定字符串开头，Output: true
 console.log(stringOps.endsWith("World!")); // 判断字符串是否以指定字符串结尾，Output: true
-console.log(stringOps.indexOf("World")); // 字符串首次出现的位置，Output: 7
 console.log(stringOps.replace("World", "Universe")); // 替换字符串，Output: Hello, Universe!
 console.log(stringOps.split(",")); // 分割字符串为数组，Output: ["Hello", " World!"]
 console.log(stringOps.trim()); // 删除字符串两端的空格，Output: Hello, World!
+console.log(stringOps.search("World")); // 查找字符串 search支持正则表达式
+console.log(stringOps.indexOf("World")); // 字符串首次出现的位置，
+console.log(str.match(/o/g)); // 查找所有的o，Output: ["o", "o"]， match支持正则表达式
 
+template = `${stringOps.toLowerCase()}, ${person.getName()}!`;//模板字符串, 反引号,${}包裹变量
+console.log(template); // Output: hello,world!, john!
+var a=0.1+0.2;
+console.log(a);
 
-
-
+let number_methods = {
+    toString: function () {//返回数字的字符串表示
+        return this.value.toString();
+    },
+    toFixed: function (n) {//返回数字的字符串表示，保留n位小数
+        return this.value.toFixed(n);
+    },
+    toPrecision: function (n) {//返回数字的字符串表示，保留n位有效数字
+        return this.value.toPrecision(n);
+    },
+    valueOf: function () {  //返回数字的原始值
+        return this.value;
+    },
+    toExponential: function (n) {//返回数字的字符串表示，使用指数计数法
+        return this.value.toExponential(n);
+    }
+};
+let number = 100;
+let number_met = Object.create(number_methods);//object.create创建一个新对象，使用现有的对象来提供新创建的对象的__proto__。new创建对象时，会调用构造函数，而Object.create不会调用构造函数
+number_met.value = number;
+console.log(number_met.toString());
+console.log(number_met.toFixed(2));
+console.log(number_met.toPrecision(2));
+console.log(number_met.valueOf());
+console.log(number_met.toExponential(2));
