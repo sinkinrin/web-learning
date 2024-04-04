02a4c25f7eef16a777a689f852a43c905c4de850
sinkinrin <1569675101@qq.com>
2024-04-04 20:06:33 +0800

    add modules and json，need to see more

---
+++ js-learning/modules.js
@@ -0,0 +1,13 @@
+{
+"copilot":{
+
+    "name": "GitHub Copilot",
+    "role": "AI Programming Assistant",
+    "expertise": "JavaScript and software development",
+    "location": "Virtual",
+    "experience": "World-class"
+    }
+}
+//
+copilot.founders = ["Nat Friedman", "Tom Preston-Werner", "Chris Wanstrath"];//添加新属性
+delete copilot.experience;//删除属性
